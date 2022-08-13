82212602896bb990915d62fc7b8600272a2cc675
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-08-13 11:23:53 +0500

    cmt

---
+++ typescript/app1.js
@@ -0,0 +1,16 @@
+// var Name:string= "hadi";
+// var num:number=17
+// var num1:number=7;
+// if(num>num1 ){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
+//  num>num1 || num<num1 
+var age = 17;
+var personatage = 50;
+age /= personatage;
+// var res:boolean=age>20 && personatage>59;
+// var res1:boolean=age>20 || personatage>49;
+console.log(age);
+// console.log(res1) 
---
+++ typescript/app1.ts
@@ -0,0 +1,23 @@
+
+// var Name:string= "hadi";
+// var num:number=17
+// var num1:number=7;
+
+// if(num>num1 ){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
+
+//  num>num1 || num<num1 
+
+
+var age:number=17
+var personatage:number=50
+
+ age /= personatage;
+
+// var res:boolean=age>20 && personatage>59;
+// var res1:boolean=age>20 || personatage>49;
+console.log(age) 
+// console.log(res1) 
