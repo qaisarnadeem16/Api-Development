278a59fed371b1ec3178cb9cc3d5b27833c2ca70
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-08-16 23:12:55 +0500

    cmmt

---
+++ typescript/new.js
@@ -0,0 +1,21 @@
+// var Grade:string="B";
+// var Grade:string="A";
+var Grade = "C";
+switch (Grade) {
+    case 'A':
+        console.log("Good Job");
+        break;
+    case 'B':
+        console.log("nyc Job");
+        break;
+    case 'C':
+        console.log("NOT Good Job");
+        break;
+    case 'D':
+        console.log("bd Job");
+        break;
+    default:
+        console.log("unknow grade");
+        break;
+}
+// console.log(Name)
---
+++ typescript/new.ts
@@ -0,0 +1,23 @@
+
+
+// var Grade:string="B";
+// var Grade:string="A";
+var Grade:string="C";
+
+switch (Grade)
+{
+    case 'A':console.log("Good Job") 
+    break;
+    case 'B':console.log("nyc Job") 
+    break;
+    case 'C':console.log("NOT Good Job") 
+    break;
+    case 'D':console.log("bd Job") 
+    break;
+    default: console.log("unknow grade")
+    break;
+}
+
+
+
+// console.log(Name)
