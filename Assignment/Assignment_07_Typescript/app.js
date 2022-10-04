8b28522448b7988d818ea8cb92a17b1320592a67
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-10-05 00:48:29 +0500

    update

--- Assignment/Assignment_07_Typescript/app.js
+++ Assignment/Assignment_07_Typescript/app.js
@@ -79,7 +79,7 @@ var countVowel = function (str) {
     }
     console.log("Vowels  ".concat(vowel, " || consonants  ").concat(consonant));
 };
-countVowel('typescript the best');
+// countVowel('typescript the best')
 // 10. Write a ts program to count total number of words in a string.
 var countWord = function (str) {
     var word = 0;
@@ -163,14 +163,418 @@ var getMax = function (str) {
 };
 //   getMax('gill')
 // 19. Write a ts program to find lowest frequency character in a string.
-var getMini = function (str) {
-    var max = 0, minChar = '';
-    str.split('').forEach(function (char) {
-        if (str.split(char).length > max) {
-            max = str.split(char).length;
-            minChar = char;
+// Write a ts program to count frequency of each character in a string.
+function getFrequency(str) {
+    var freq = {};
+    for (var i = 0; i < str.length; i++) {
+        var character = str.charAt(i);
+        if (freq[character]) {
+            freq[character]++;
         }
-    });
-    console.log(minChar, max);
+        else {
+            freq[character] = 1;
+        }
+    }
+    console.log(freq);
+}
+;
+// getFrequency("qaisar gill")
+// 21. Write a ts program to remove first occurrence of a character from string.
+var removeFirstChar = function (str) {
+    var arr = str.split(' ');
+    arr.shift();
+    var str2 = arr.join('  ');
+    console.log(str2);
 };
-getMini('gill');
+// removeFirstChar('qaisar gill 11')
+// 22. Write a ts program to remove last occurrence of a character from string.
+var removelLastChar = function (str) {
+    var arr = str.split(' ');
+    arr.pop();
+    var str2 = arr.join('  ');
+    console.log(str2);
+};
+// removelLastChar('qaisar gill 11')
+// 23. Write a ts program to remove all occurrences of a character from string.
+var removelAllChar = function (str) {
+    // let arr=str.split(' ')
+    var arr = str.slice(0, 0);
+    // let str2=arr.join('  ')
+    console.log(arr);
+};
+// removelAllChar('qaisar gill 11')
+// 24. Write a ts program to remove all repeated characters from a given string.
+var removeRepeated = function (str) {
+    // str=Array.from(new Set(str.split(''))).join('');
+    console.log(str);
+};
+// removeRepeated('qqaaqrmj')
+// 25. Write a ts program to replace first occurrence of a character with another in a string.
+var replaceFirst = function (str, replaceTo) {
+    var arr = str.split('');
+    arr.splice(0, 1, replaceTo);
+    var str2 = arr.join('');
+    console.log(str2);
+};
+// replaceFirst('qaisar','f')
+// 26. Write a ts program to replace last occurrence of a character with another in a string.
+var replaceLast = function (str, replaceTo) {
+    var arr = str.split('');
+    arr.splice(-1, 1, replaceTo);
+    var str2 = arr.join('');
+    console.log(str2);
+};
+// replaceLast('qaisar','f')
+// 27. Write a ts program to replace all occurrences of a character with another in a string.
+var replaceAll = function (str, replaceTo) {
+    console.log("String Before replace ".concat(str));
+    var b = str.replace(str, replaceTo);
+    console.log("String After replace ".concat(b));
+};
+// replaceAll('gill','qaisar')
+// 28. Write a ts program to find first occurrence of a word in a given string.
+var findFirst = function (str, target) {
+    var str2 = str.indexOf(target);
+    console.log(str2);
+};
+// findFirst('qaisar gill','s')
+// 29. Write a ts program to find last occurrence of a word in a given string.
+var findLast = function (str, target) {
+    var str2 = str.lastIndexOf(target);
+    console.log(str2);
+};
+//  findLast('qaisar gill','i')
+//  30. Write a ts program to search all occurrences of a word in given string.
+var searchall = function (str, target) {
+    if (str.search(target) == -1) {
+        console.log("Word is not found");
+    }
+    else {
+        console.log("word is at this index ".concat(str.search(target)));
+    }
+};
+//  searchAll("hi there qaisar gill " ,'gill')
+// 31. Write a ts program to count occurrences of a word in a given string.
+var countSearchWord = function (str) {
+    var str2 = (str.match(/gill/g) || []).length;
+    console.log(str2);
+};
+//  countSearchWord("hi there qaisar gill gill")
+// 32. Write a ts program to remove first occurrence of a word from string 
+var removeFirstWord = function (str) {
+    var arr = str.split(' ');
+    arr.shift();
+    var str2 = arr.join(' ');
+    console.log(str2);
+};
+// removeFirstWord('hadi gill')
+// 33. Write a ts program to remove last occurrence of a word in given string.
+var removeLastWord = function (str) {
+    var arr = str.split(' ');
+    arr.pop();
+    var str2 = arr.join(' ');
+    console.log(str2);
+};
+// removeLastWord('hadi gill')
+// 34. Write a ts program to remove all occurrence of a word in given string
+var removeAllWord = function (str) {
+    console.log(str.slice(0, 0));
+};
+// removeAllWord('hadi gill')
+// 35. Write a ts program to trim leading white space characters from given string.
+var trimLeading = function (str) {
+    var str2 = str.trimStart();
+    console.log((str2));
+};
+// trimLeading("         hllkwkw       ")
+// 35. Write a ts program to trim Trailing white space characters from given string.
+var trimTrailing = function (str) {
+    var str2 = str.trimStart();
+    console.log((str2));
+};
+// trimTrailing("         hllkwkw       ")
+// 37. Write a ts program to trim both leading and trailing white space characters from given string.
+var trim = function (str) {
+    var str2 = str.trim();
+    console.log((str2));
+};
+// trim("         hllkwkw       ")
+// 38. Write a ts program to remove all extra blank spaces from given string.
+var removeWhiteSpace = function (str) {
+    var str2 = str.replace(/\s/g, '');
+    console.log(str2);
+};
+// removeWhiteSpace(' hjdf haJH  HEahi  iefUI  IF JBJA   J')
+// conditional operators programming exercises
+// 1. Write a ts program to find maximum between two numbers using conditional operator.
+var maxiNum = function (num1, num2) {
+    if (num1 > num2) {
+        console.log("Number : ".concat(num1, " is greater then Number : ").concat(num2));
+    }
+    else {
+        console.log("Number : ".concat(num1, " is Smaller then Number : ").concat(num2));
+    }
+};
+// maxiNum(22,121)
+// 2. Write a ts program to find maximum between three numbers using conditional operator.
+var max3Num = function (num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        console.log("Number : ".concat(num1, " is greater then : ").concat(num2, " : ").concat(num3));
+    }
+    else if (num2 > num1 && num2 > num3) {
+        console.log("Number : ".concat(num2, " is greater then : ").concat(num1, " : ").concat(num3));
+    }
+    else if (num3 > num1 && num3 > num2) {
+        console.log("Number : ".concat(num3, " is greater then : ").concat(num2, " : ").concat(num1));
+    }
+};
+// max3Num(5,2,3)
+// 3. Write a ts program to check whether a number is even or odd using conditional operator.
+var checkEvenOdd = function (number) {
+    if (number % 2 == 0) {
+        console.log("".concat(number, " is even "));
+    }
+    else {
+        console.log("".concat(number, " is odd "));
+    }
+};
+// checkEvenOdd(2)
+// 4. Write a ts program to check whether year is leap year or not using conditional operator.
+var checkLeap = function (year) {
+    if (year % 400 == 0) {
+        console.log("".concat(year, " is a leap year"));
+    }
+    else if (year % 100 == 0) {
+        console.log("".concat(year, " is a Not  leap year"));
+    }
+    else if (year % 4 == 0) {
+        console.log("".concat(year, " is a leap year"));
+    }
+    else {
+        console.log("".concat(year, " is a not leap year"));
+    }
+};
+// checkLeap(2006)
+// 5. Write a ts program to check whether character is an alphabet or not using conditional operator.
+var checkAlphbet = function (char) {
+    if (char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z') {
+        console.log("".concat(char, "  : is Alphabet"));
+    }
+    else {
+        console.log("".concat(char, "  : is Not Alphabet"));
+    }
+};
+// checkAlphbet('')
+// List of switch case programming exercises
+// 1. Write a ts program to print day of week name using switch case.
+var weekDay = function (day) {
+    switch (day) {
+        case 1: {
+            console.log('Monday');
+            break;
+        }
+        case 2: {
+            console.log('Tuesday');
+            break;
+        }
+        case 3: {
+            console.log('Wednesday');
+            break;
+        }
+        case 4: {
+            console.log('Thurday');
+            break;
+        }
+        case 5: {
+            console.log('Friday');
+            break;
+        }
+        case 6: {
+            console.log('Saturday');
+            break;
+        }
+        case 7: {
+            console.log('Sunday');
+            break;
+        }
+        default: {
+            console.log("Invalid choice");
+            break;
+        }
+    }
+};
+// weekDay()
+// 2. Write a ts program print total number of days in a month using switch case.
+var dayInMonth = function (Month) {
+    switch (Month) {
+        case 'January':
+            console.log("31 days");
+            break;
+        case 'February':
+            console.log("28/29 days");
+            break;
+        case 'March':
+            console.log("31 days");
+            break;
+        case 'April':
+            console.log("30 days");
+            break;
+        case 'May':
+            console.log("31 days");
+            break;
+        case 'June':
+            console.log("30 days");
+            break;
+        case 'July':
+            console.log("31 days");
+            break;
+        case 'August':
+            console.log("31 days");
+            break;
+        case 'September':
+            console.log("30 days");
+            break;
+        case 'October':
+            console.log("31 days");
+            break;
+        case 'November':
+            console.log("30 days");
+            break;
+        case 'December':
+            console.log("31 days");
+            break;
+        default:
+            console.log();
+            ("Invalid input! Please enter month  between 1-12");
+    }
+};
+// dayInMonth('March')
+// 3. Write a ts program to check whether an alphabet is vowel or consonant using switch case.
+var checkVowel = function (Char) {
+    switch (Char) {
+        case 'a': {
+            console.log("alphabet is vowel");
+            break;
+        }
+        case 'e': {
+            console.log("alphabet is vowel");
+            break;
+        }
+        case 'i': {
+            console.log("alphabet is vowel");
+            break;
+        }
+        case 'o': {
+            console.log("alphabet is vowel");
+            break;
+        }
+        case 'u': {
+            console.log("alphabet is vowel");
+            break;
+        }
+        default: {
+            console.log("alphabet is Consonant");
+            break;
+        }
+    }
+};
+// checkVowel('d')
+// 4. Write a ts program to find maximum between two numbers using switch case.
+var checkGreater = function (num1, num2) {
+    switch (num1 > num1) {
+        case true: {
+            console.log("".concat(num1, " is Greater then ").concat(num2));
+        }
+        case false: {
+            console.log("".concat(num2, " is Greater then ").concat(num1));
+        }
+    }
+};
+// checkGreater(2,4)
+// 5. Write a ts program to check whether a number is even or odd using switch case.
+var checkOddEven = function (num) {
+    switch (num % 2 == 0) {
+        case true: {
+            console.log("".concat(num, "  is Even"));
+        }
+        case false: {
+            console.log("".concat(num, "  is Odd"));
+        }
+    }
+};
+// checkOddEven(9)
+// 6. Write a ts program to check whether a number is positive, negative or zero using switch case.
+var checkNumber = function (num) {
+    switch (num > 0) {
+        case true: {
+            console.log("".concat(num, " is Postive"));
+            break;
+        }
+        case false: {
+            switch (num < 0) {
+                case true: {
+                    console.log("".concat(num, " is Negative"));
+                    break;
+                }
+                case false: {
+                    console.log("".concat(num, " is Zero"));
+                    break;
+                }
+            }
+        }
+    }
+};
+// checkNumber(0)
+// 7. Write a ts program to find roots of a quadratic equation using switch case.
+var rootsOfQuaduratic = function (a, b, c) {
+    var dis = (b * b) - (4 * a * c);
+    var root1, root2;
+    switch (dis > 0) {
+        case true: {
+            root1 = (-b + Math.sqrt(dis)) / (2 * a);
+            root2 = (-b - Math.sqrt(dis)) / (2 * a);
+            console.log("\"Two distinct and real roots exists: \", ".concat(root1, ", ").concat(root2));
+            break;
+        }
+        case false: {
+            switch (dis < 0) {
+                case true: {
+                    root1 = root2 = (-b - Math.sqrt(dis)) / (2 * a);
+                    var imag = (-dis) / (2 * a);
+                    console.log("\"Two distinct complex roots exist: \", ".concat(root1, ", ").concat(root2, " , ").concat(imag));
+                    break;
+                }
+                case false: {
+                    root1 = root2 = -b / (2 * a);
+                    console.log("Two equal and real roots exists;");
+                    break;
+                }
+            }
+        }
+    }
+};
+// rootsOfQuaduratic(9,4,5)
+// 8. Write a ts program to create Simple Calculator using switch case
+var Calculator = function (num1, operator, num2) {
+    var result;
+    switch (operator) {
+        case '+': {
+            result = num1 + num2;
+            break;
+        }
+        case '-': {
+            result = num1 - num2;
+            break;
+        }
+        case '*': {
+            result = num1 * num2;
+            break;
+        }
+        case '/': {
+            result = num1 / num2;
+            break;
+        }
+    }
+    console.log("".concat(num1, " ").concat(operator, " ").concat(num2, "= ").concat(result));
+};
+Calculator(3, '*', 2);
--- Assignment/Assignment_07_Typescript/app.ts
+++ Assignment/Assignment_07_Typescript/app.ts
@@ -93,7 +93,7 @@ let countVowel=(str:string)=>{
    }
    console.log(`Vowels  ${vowel} || consonants  ${consonant}`);
 }
-countVowel('typescript the best')
+// countVowel('typescript the best')
 
 // 10. Write a ts program to count total number of words in a string.
 let  countWord=(str:string)=>{
@@ -188,18 +188,443 @@ let  getMax =(str:string)=>{
     });
     console.log(maxChar  , max);
   };
-
 //   getMax('gill')
 // 19. Write a ts program to find lowest frequency character in a string.
-let  getMini =(str:string)=>{
-   var max = 0,  minChar = '';
-    str.split('').forEach(function(char){
-      if(str.split(char).length > max) {
-          max = str.split(char).length;
-          minChar = char;
-       }
-    });
-    console.log(minChar  , max);
-  };
 
-  getMini('gill')
+
+
+
+// Write a ts program to count frequency of each character in a string.
+function getFrequency(str:string) {
+   var freq = {};
+   for (var i=0; i<str.length;i++) {
+       var character = str.charAt(i);
+       if (freq[character]) {
+          freq[character]++;
+       } else {
+          freq[character] = 1;
+       }  
+   }
+   console.log(freq);  
+ 
+};
+// getFrequency("qaisar gill")
+// 21. Write a ts program to remove first occurrence of a character from string.
+let removeFirstChar=(str:string)=>{
+   let arr=str.split(' ')
+   arr.shift()
+   let str2=arr.join('  ')
+   console.log(str2);
+}
+// removeFirstChar('qaisar gill 11')
+// 22. Write a ts program to remove last occurrence of a character from string.
+let removelLastChar=(str:string)=>{
+   let arr=str.split(' ')
+   arr.pop()
+   let str2=arr.join('  ')
+   console.log(str2);
+}
+// removelLastChar('qaisar gill 11')
+
+// 23. Write a ts program to remove all occurrences of a character from string.
+let removelAllChar=(str:string)=>{
+   // let arr=str.split(' ')
+  let arr=str.slice(0,0)
+   // let str2=arr.join('  ')
+   console.log(arr);
+}
+// removelAllChar('qaisar gill 11')
+// 24. Write a ts program to remove all repeated characters from a given string.
+let removeRepeated=(str:string)=>{
+   // str=Array.from(new Set(str.split(''))).join('');
+   console.log(str);   
+}
+// removeRepeated('qqaaqrmj')
+// 25. Write a ts program to replace first occurrence of a character with another in a string.
+let replaceFirst=(str:string,replaceTo:string)=>{
+   let arr=str.split('')
+   arr.splice(0,1,replaceTo)
+   let str2=arr.join('')
+   console.log(str2);
+     
+}
+// replaceFirst('qaisar','f')
+// 26. Write a ts program to replace last occurrence of a character with another in a string.
+let replaceLast=(str:string,replaceTo:string)=>{
+   let arr=str.split('')
+   arr.splice(-1,1,replaceTo)
+   let str2=arr.join('')
+   console.log(str2);
+     
+}
+// replaceLast('qaisar','f')
+// 27. Write a ts program to replace all occurrences of a character with another in a string.
+let replaceAll=(str:string,replaceTo:string)=>{
+   console.log(`String Before replace ${str}`);
+   let b=str.replace(str,replaceTo)
+   console.log(`String After replace ${b}`);    
+}
+// replaceAll('gill','qaisar')
+// 28. Write a ts program to find first occurrence of a word in a given string.
+let findFirst=(str:string,target:string)=>{
+  let str2=str.indexOf(target)
+  console.log(str2);
+}
+// findFirst('qaisar gill','s')
+// 29. Write a ts program to find last occurrence of a word in a given string.
+let findLast=(str:string,target:string)=>{
+   let str2=str.lastIndexOf(target)
+   console.log(str2);
+ }
+//  findLast('qaisar gill','i')
+//  30. Write a ts program to search all occurrences of a word in given string.
+let searchall=(str:string,target:string)=>{
+   if(str.search(target)==-1){
+      console.log(`Word is not found`);   
+   }else{
+      console.log(`word is at this index ${str.search(target)}`);
+   }
+ }
+//  searchAll("hi there qaisar gill " ,'gill')
+// 31. Write a ts program to count occurrences of a word in a given string.
+let countSearchWord=(str:string)=>{
+   let str2=(str.match(/gill/g)||[]).length
+   console.log(str2);
+ }
+//  countSearchWord("hi there qaisar gill gill")
+// 32. Write a ts program to remove first occurrence of a word from string 
+let removeFirstWord=(str:string)=>{
+   let arr=str.split(' ')
+   arr.shift()
+   let str2=arr.join(' ')
+   console.log(str2);
+}
+// removeFirstWord('hadi gill')
+// 33. Write a ts program to remove last occurrence of a word in given string.
+let removeLastWord=(str:string)=>{
+   let arr=str.split(' ')
+   arr.pop()
+   let str2=arr.join(' ')
+   console.log(str2);
+}
+// removeLastWord('hadi gill')
+// 34. Write a ts program to remove all occurrence of a word in given string
+let removeAllWord=(str:string)=>{
+   console.log(str.slice(0,0)); 
+}
+// removeAllWord('hadi gill')
+// 35. Write a ts program to trim leading white space characters from given string.
+let trimLeading=(str:string)=>{
+   let str2=str.trimStart()
+   console.log((str2));  
+}
+// trimLeading("         hllkwkw       ")
+// 35. Write a ts program to trim Trailing white space characters from given string.
+let trimTrailing=(str:string)=>{
+   let str2=str.trimStart()
+   console.log((str2));  
+}
+// trimTrailing("         hllkwkw       ")
+// 37. Write a ts program to trim both leading and trailing white space characters from given string.
+let trim=(str:string)=>{
+   let str2=str.trim()
+   console.log((str2));  
+}
+// trim("         hllkwkw       ")
+
+// 38. Write a ts program to remove all extra blank spaces from given string.
+let removeWhiteSpace=(str:string)=>{
+   let str2=str.replace(/\s/g,'')
+   console.log(str2);
+   
+}
+// removeWhiteSpace(' hjdf haJH  HEahi  iefUI  IF JBJA   J')
+
+
+
+
+
+
+// conditional operators programming exercises
+
+// 1. Write a ts program to find maximum between two numbers using conditional operator.
+
+let maxiNum=(num1:number,num2:number)=>{
+   if(num1>num2){
+      console.log(`Number : ${num1} is greater then Number : ${num2}`)   
+   }else{
+      console.log(`Number : ${num1} is Smaller then Number : ${num2}`)   
+   }
+}
+// maxiNum(22,121)
+// 2. Write a ts program to find maximum between three numbers using conditional operator.
+let max3Num=(num1:number,num2:number,num3:number)=>{
+if(num1>num2 && num1>num3){
+   console.log(`Number : ${num1} is greater then : ${num2} : ${num3}`)
+}else if(num2>num1 && num2>num3){
+   console.log(`Number : ${num2} is greater then : ${num1} : ${num3}`)
+}else if(num3>num1 && num3>num2){
+   console.log(`Number : ${num3} is greater then : ${num2} : ${num1}`)
+}
+}
+// max3Num(5,2,3)
+// 3. Write a ts program to check whether a number is even or odd using conditional operator.
+let checkEvenOdd=(number:number)=>{
+   if(number%2==0){
+      console.log(`${number} is even `);
+   }else{
+      console.log(`${number} is odd `);
+   }
+}
+// checkEvenOdd(2)
+// 4. Write a ts program to check whether year is leap year or not using conditional operator.
+
+let checkLeap=(year:number)=>{
+   if (year % 400 == 0) {
+      console.log(`${year} is a leap year`);
+    }else if (year % 100 == 0) {
+      console.log(`${year} is a Not  leap year`);
+    }else if (year % 4 == 0) {
+      console.log(`${year} is a leap year`);
+    }
+    else {
+      console.log(`${year} is a not leap year`);
+    }
+}
+// checkLeap(2006)
+// 5. Write a ts program to check whether character is an alphabet or not using conditional operator.
+let checkAlphbet=(char:string)=>{
+   if(char>='a' && char<='z' ||char>='A' && char<='Z' ){
+      console.log(`${char}  : is Alphabet`); 
+   }else{
+      console.log(`${char}  : is Not Alphabet`); 
+   }
+}
+// checkAlphbet('')
+
+// List of switch case programming exercises
+
+// 1. Write a ts program to print day of week name using switch case.
+
+  let weekDay=(day:number)=>{
+   switch (day){
+      case 1:{
+         console.log('Monday');
+         break
+      }
+      case 2:{
+         console.log('Tuesday');
+         break
+      }
+      case 3:{
+         console.log('Wednesday');
+         break
+      }
+      case 4:{
+         console.log('Thurday');
+         break
+      }
+      case 5:{
+         console.log('Friday');
+         break
+      }
+      case 6:{
+         console.log('Saturday');
+         break
+      }
+      case 7:{
+         console.log('Sunday');
+         break
+      }
+      default:{
+         console.log("Invalid choice");
+         break;
+         }
+   }
+  }
+
+// weekDay()
+// 2. Write a ts program print total number of days in a month using switch case.
+
+let dayInMonth=(Month:string)=>{
+   switch(Month){
+      case 'January': 
+      console.log("31 days");
+      break;
+  case 'February': 
+      console.log("28/29 days");
+      break;
+  case 'March': 
+      console.log("31 days");
+      break;
+  case 'April': 
+      console.log("30 days");
+      break;
+  case 'May': 
+      console.log("31 days");
+      break;
+  case 'June': 
+      console.log("30 days");
+      break;
+  case 'July': 
+      console.log("31 days");
+      break;
+  case 'August': 
+      console.log("31 days");
+      break;
+  case 'September': 
+      console.log("30 days");
+      break;
+  case 'October': 
+      console.log("31 days");
+      break;
+  case 'November': 
+      console.log("30 days");
+      break;
+  case 'December': 
+      console.log("31 days");
+      break;
+  default: 
+      console.log();
+      ("Invalid input! Please enter month  between 1-12")
+   }
+}
+// dayInMonth('March')
+// 3. Write a ts program to check whether an alphabet is vowel or consonant using switch case.
+
+let checkVowel=(Char:string)=>{
+   switch(Char){
+     case 'a':{
+      console.log("alphabet is vowel");
+      break
+     }
+     case 'e':{
+      console.log("alphabet is vowel");
+      break
+     }
+     case 'i':{
+      console.log("alphabet is vowel");
+      break
+     } case 'o':{
+      console.log("alphabet is vowel");
+      break
+     } case 'u':{
+      console.log("alphabet is vowel");
+      break
+     }
+     default:{
+      console.log("alphabet is Consonant");
+      break
+     }
+   }
+}
+// checkVowel('d')
+// 4. Write a ts program to find maximum between two numbers using switch case.
+let checkGreater=(num1:number,num2:number)=>{
+  
+   switch(num1>num1){
+      case true:{
+       console.log(`${num1} is Greater then ${num2}`);
+      }
+      case false:{
+         console.log(`${num2} is Greater then ${num1}`);
+        }
+   }
+}
+// checkGreater(2,4)
+// 5. Write a ts program to check whether a number is even or odd using switch case.
+let checkOddEven=(num:number)=>{
+   switch(num%2==0){
+      case true:{
+       console.log(`${num}  is Even`);
+      }
+      case false:{
+         console.log(`${num}  is Odd`);
+        }
+   }
+}
+// checkOddEven(9)
+// 6. Write a ts program to check whether a number is positive, negative or zero using switch case.
+let checkNumber=(num:number)=>{
+   switch(num>0){
+      case true:{
+       console.log(`${num} is Postive`);
+       break
+      }
+      case false:{
+             switch(num<0){
+                  case true:{
+                  console.log(`${num} is Negative`);
+                  break
+                  }
+                  case false:{
+                  console.log(`${num} is Zero`);
+                  break
+               }
+               }
+      }
+     
+   }
+}
+// checkNumber(0)
+// 7. Write a ts program to find roots of a quadratic equation using switch case.
+let rootsOfQuaduratic=(a:number,b:number,c:number)=>{
+   let dis=(b*b)-(4*a*c);
+   let root1,root2
+   
+   switch(dis>0){
+      case true:{
+         root1 = (-b + Math.sqrt(dis)) / (2 * a);
+         root2 = (-b - Math.sqrt(dis)) / (2 * a);
+
+         console.log(`"Two distinct and real roots exists: ", ${root1}, ${root2}`);
+       break;
+      }
+      case false:{
+             switch(dis<0){
+               case true:{
+               root1 = root2 = (-b - Math.sqrt(dis)) / (2 * a);
+              let imag = (-dis) / (2 * a);
+      
+               console.log(`"Two distinct complex roots exist: ", ${root1}, ${root2} , ${imag}`);
+             break;
+               }
+                  case false:{
+                     root1 = root2 = -b / (2 * a);
+
+                     console.log ("Two equal and real roots exists;")
+ 
+                     break
+               }
+               }
+      }
+     
+   }
+}
+// rootsOfQuaduratic(9,4,5)
+// 8. Write a ts program to create Simple Calculator using switch case
+
+let Calculator=(num1:number,operator,num2:number)=>{
+  let result;
+   switch(operator){
+      case '+':{
+       result=num1+num2
+       break;
+      }
+      case '-':{
+         result=num1-num2
+         break
+        }
+      case '*':{
+         result=num1*num2
+         break
+   }
+   case '/':{
+      result=num1/num2
+      break
+     }
+}
+console.log(`${num1} ${operator} ${num2}= ${result}`);
+
+}
+Calculator(3,'*',2)
