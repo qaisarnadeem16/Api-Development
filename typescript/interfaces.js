07c7f8bef9ecae65d02730a2fb86a8dce2bc4faa
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-10-10 23:57:07 +0500

    update

--- typescript/interfaces.js
+++ typescript/interfaces.js
@@ -1,3 +1,20 @@
 var person = { name: 'ali', age: 22, fname: 'ahmad', sayHi: function () { return 'hello'; }
 };
 console.warn(person);
+var Car = /** @class */ (function () {
+    //constructor
+    function Car(engine) {
+        this.engine = engine;
+    }
+    //function
+    Car.prototype.disp = function () {
+        console.log("Function displays Engine is : " + this.engine);
+    };
+    return Car;
+}());
+//create an object
+var obj = new Car("XXSY1");
+//access the field
+// console.log("Reading attribute value Engine as : "+obj.engine)
+//access the function
+obj.disp();
--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -9,3 +9,32 @@ var person:p={name:'ali',age:22,fname:'ahmad',sayHi:()=>{return 'hello'};
     
 
 console.warn(person);
+// console.warn(person.sayHi());
+interface RunOptions
+{
+program:string;
+commandLine:string[]|string|(()=>string);
+}
+
+
+
+class Car
+{ //field
+engine:string;
+//constructor
+constructor(engine:string)
+{
+this.engine=engine
+}
+//function
+disp():void
+{
+console.log("Function displays Engine is : "+this.engine)
+}
+}
+//create an object
+var obj= new Car("XXSY1")
+//access the field
+// console.log("Reading attribute value Engine as : "+obj.engine)
+//access the function
+obj.disp()
